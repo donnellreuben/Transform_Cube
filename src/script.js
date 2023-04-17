66c64f3882f5b0978efa3cb8e00d5d4ef40d23e7
Donnell Reuben <donnell.reuben@icloud.com>
2023-04-17 15:33:19 -0500

    Added Axes helper and initialized server

---
+++ src/script.js
@@ -0,0 +1,56 @@
+import * as THREE from 'three'
+
+// Canvas
+const canvas = document.querySelector('canvas.webgl')
+
+// Scene
+const scene = new THREE.Scene()
+
+/**
+ * Objects
+ */
+const geometry = new THREE.BoxGeometry(1, 1, 1)
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+const mesh = new THREE.Mesh(geometry, material)
+scene.add(mesh)
+
+// Position
+// mesh.position.x = - 0.6
+// mesh.position.y = 0.6
+// mesh.position.z = 1
+mesh.position.set(0.7, -0.6, 1)
+
+// Scale
+
+
+// Axes helper (helps position objects in space)
+const axesHelper = new THREE.AxesHelper(2)
+scene.add(axesHelper)
+
+/**
+ * Sizes
+ */
+const sizes = {
+    width: 800,
+    height: 600
+}
+
+/**
+ * Camera
+ */
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
+camera.position.z = 3
+
+
+
+
+scene.add(camera)
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas
+})
+renderer.setSize(sizes.width, sizes.height)
+renderer.render(scene, camera)
