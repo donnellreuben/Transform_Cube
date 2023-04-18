f1d91a5804e7739dfd912e7e17a3e93e05862784
Donnell Reuben <donnell.reuben@icloud.com>
2023-04-18 15:23:36 -0500

    Made 3 cubes and added them to a group

--- src/script.js
+++ src/script.js
@@ -9,28 +9,32 @@ const scene = new THREE.Scene()
 /**
  * Objects
  */
-const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
-const mesh = new THREE.Mesh(geometry, material)
-scene.add(mesh)
+const group = new THREE.Group()
+group.position.y = 1
+group.scale.y = 2
+group.rotation.y = 1
+scene.add(group)
 
-// Position
-// mesh.position.x = - 0.6
-// mesh.position.y = 0.6
-// mesh.position.z = 1
-mesh.position.set(0.7, -0.6, 1)
+const cube1 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial({color: 0xff0000 })
+)
+group.add(cube1)
 
-// Scale
-// mesh.scale.x = 2
-// mesh.scale.y = 0.5
-// mesh.scale.z = 0.5
-mesh.scale.set(2, 0.5, 0.5)
+const cube2 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial({color: 0x00ff00 })
+)
+cube2.position.x = -2
+group.add(cube2)
 
-// Rotation 
-// mesh.rotation.y = 3.14159
-mesh.rotation.reorder('YXZ')
-mesh.rotation.x = Math.PI * 0.25
-mesh.rotation.y = Math.PI * 0.25
+const cube3 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial({color: 0x0000ff })
+)
+cube3.position.x = 2
+group.add(cube3)
+
 
 // Axes helper (helps position objects in space)
 const axesHelper = new THREE.AxesHelper(1)
@@ -49,6 +53,8 @@ const sizes = {
  */
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
 camera.position.z = 3
+
+
 
 
 
